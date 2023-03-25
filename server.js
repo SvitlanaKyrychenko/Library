14747cb321632e0b3a9c07830af9c93677da8d86
Alex-6346 <66250635+Alex-6346@users.noreply.github.com>
2023-03-25 17:19:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,22 @@ const express = require('express');
 
 const app = express();
 
-app.get('/api/authors', (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-  ];
+const authors = [ 
+{id: 1, name: 'John Silver'},
+{id: 2, name: 'J.K.R'},
+{id: 3, name: 'Richard Duhkins'}
+]
 
-  res.json(customers);
+const books = [
+  {id:1, author_id: 1, title: "John FB", genre:"science"},
+  {id:2, author_id: 1, title: "John SB", genre:"detective"},
+  {id:3, author_id: 2, title: "J.K.R FB", genre:"science"},
+  {id:4, author_id: 2, title: "J.K.R SB", genre:"adventure"},
+  {id:5, author_id: 3, title: "Richard FB", genre:"fiction"},
+  {id:6, author_id: 3, title: "Richard SB", genre:"adventure"}
+]
+
+  res.json(authors);
 });
 
 const port = 5000;
